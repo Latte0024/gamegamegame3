229e4ff9f05d706156892fb9f7535a903a0d04df
zeronic712 <zeronic@mailbox.org>
2026-05-23 13:23:52 +0200

    raul beam
    :)

--- scripts/battle/battleManager.gd
+++ scripts/battle/battleManager.gd
@@ -3,6 +3,8 @@ extends Control
 
 @onready var text = $ui/status/margin/battleLog
 @onready var skillmenu = $ui/status/margin/skills
+
+var canCycle:bool = true
 ## who the skill tagets
 enum target {
 	## user of spell only, skips target confirmation
@@ -75,7 +77,7 @@ func _process(delta: float) -> void:
 		text.visible = true
 
 func _init_battle() -> void:
-	
+	canCycle = false
 	visible = !visible
 	
 	text.append_text("[i]* FOES DRAW NEAR !! [/i]")
@@ -85,6 +87,7 @@ func _init_battle() -> void:
 		
 		var enemySprite = preload("res://scripts/battle/scenes/enemy.tscn").instantiate()
 		enemySprite.texture_normal = enemy.texture 
+		enemySprite.damageVal = enemy.damage 
 		enemySprite.battleHandler = self
 		$enemies.add_child(enemySprite)
 		enemySprite.name = enemy.name
@@ -113,6 +116,9 @@ func _init_battle() -> void:
 
 
 func _player_turn():
+	if canCycle:
+		return
+		
 	_enemy_check()
 	print("enemy check at start of player turn")
 	
@@ -165,9 +171,12 @@ func _player_turn():
 
 
 func _enemy_check():
+	if canCycle:
+		return
 	
 	await get_tree().create_timer(.3).timeout
 	if $enemies.get_children().is_empty() and !checked:
+		canCycle = true
 		checked = true
 		text.newline()
 		text.newline()
@@ -177,6 +186,9 @@ func _enemy_check():
 		
 
 func _enemy_turn():
+	if canCycle:
+		return
+		
 	_enemy_check()
 	
 	for i in $ui/btn.get_children():
@@ -191,7 +203,7 @@ func _enemy_turn():
 	
 	if !$enemies.get_children().is_empty():
 		for enemy in $enemies.get_children().size():
-			print($enemies.get_children()[enemy].name, ": geci sexy vagyok")
+			$ui/party.get_children().pick_random().damage($enemies.get_child(enemy).damageVal,$enemies.get_child(enemy).name)
 	
 	_player_turn()
 
@@ -221,6 +233,9 @@ func _battle_end():
 		
 		
 func _target_select(targeting:target, effect:int, scaling:float, cost:int):
+	if canCycle:
+		return
+	
 	var damageCalc = effect * (1 + (scaling * BattleGlobals.partyLevel))
 	match targeting:
 		0: #me
@@ -263,8 +278,8 @@ func _target_select(targeting:target, effect:int, scaling:float, cost:int):
 				i.set_focus_mode(FOCUS_ALL)
 				i.disabled = false
 			
-			
-			$enemies.get_child(0).grab_focus()
+			if !$enemies.get_children().is_empty():
+				$enemies.get_child(0).grab_focus()
 		
 		4: #foes
 			for i in $ui/btn.get_children():
--- scripts/battle/classes/enemyData.gd
+++ scripts/battle/classes/enemyData.gd
@@ -4,3 +4,4 @@ class_name enemyData
 @export_placeholder("Juan") var name : String
 @export var texture : Texture2D
 @export var health : int = 100
+@export var damage : int = 100
